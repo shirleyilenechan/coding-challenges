af5e7645e14fc2b4f6a2216436e7ffc4b089d7dd
Shirley Chan <shirleyilenechan@gmail.com>
2018-09-04 03:30:42 +0000

    urlify

---
+++ urlify.py
@@ -0,0 +1,9 @@
+# replace spaces in string with %20;
+# string has additional space in the end to hold additional characters
+
+
+def urlify(string):
+    new_string = string.rstrip().lstrip()
+    new_string = new_string.split()
+    new_string = "%20".join(new_string)
+    return new_string
