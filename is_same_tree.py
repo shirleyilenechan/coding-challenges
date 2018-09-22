0d873cdcc6440dda5df846dc6bae7c6054290c05
Shirley Chan <shirleyilenechan@gmail.com>
2018-09-22 00:09:08 +0000

    is same tree

---
+++ is_same_tree.py
@@ -0,0 +1,9 @@
+def is_same_tree(p, q):
+    if p is None and q is None:
+        return True
+    if p is None or q is None:
+        return False
+    if p.val != q.val:
+        return False
+
+    return is_same_tree(p.left, q.left) and is_same_tree(p.right, q.right)
