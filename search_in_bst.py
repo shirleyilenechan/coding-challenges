19ef0f136213a8472eb58b0d7207c7df266b770a
Shirley Chan <shirleyilenechan@gmail.com>
2018-09-22 00:16:38 +0000

    search in bst

--- search_in_bst.py
+++ search_in_bst.py
@@ -5,6 +5,6 @@ def search_bst(root, val):
         if root.val > val:
             return search_bst(root.left, val)
         elif root.val < val:
-            return search_bST(root.right, val)
+            return search_bst(root.right, val)
         else:
             return root
