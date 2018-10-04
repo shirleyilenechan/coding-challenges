4cfa06bed72398056ac9cb39ee6bf073ab1bfaf7
Shirley Chan <shirleyilenechan@gmail.com>
2018-10-04 22:34:51 +0000

    sort array by parity

--- sort_array_by_parity.py
+++ sort_array_by_parity.py
@@ -1,4 +1,4 @@
 def sort_array_by_parity(A):
-        even_list = [num for num in A if num % 2 == 0]
-        odd_list = [num for num in A if num % 2 != 0]
-        return even_list + odd_list
+    even_list = [num for num in A if num % 2 == 0]
+    odd_list = [num for num in A if num % 2 != 0]
+    return even_list + odd_list
